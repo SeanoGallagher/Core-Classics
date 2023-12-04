a4ac1cb32c6c1274c8ebe7c76120dbf7947525d4
ctuttle03 <ctuttle03@gmail.com>
2023-12-03 16:44:37 -0800

    Flappy Bird all

--- Assets/Flappy Bird/Assets/Score.cs
+++ Assets/Flappy Bird/Assets/Score.cs
@@ -7,11 +7,14 @@ public class Score : MonoBehaviour
     public static int score = 0;
 
     private void Start(){
-        score = 0;
+
     }
 
     private void Update(){
-        
         GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+    }
+
+    public void ResetScore(){
+        score=0;
     }
 }
