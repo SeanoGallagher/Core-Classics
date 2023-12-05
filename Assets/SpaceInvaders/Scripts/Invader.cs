23f413a3ed8d16566db7bcbcf9e5ae954bc4333c
gabrielv177 <152953538+gabrielv177@users.noreply.github.com>
2023-12-04 23:07:39 -0800

    Space Invaders

---
+++ Assets/SpaceInvaders/Scripts/Invader.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Invader : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+    public float animationTime = 1f;
+
+    public System.Action killed;
+    //public int score = 10;
+
+    private SpriteRenderer _spriteRenderer;
+    private int _animationFrame;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+    }
+
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+
+        // Loop back to the start if the animation frame exceeds the length
+        if (_animationFrame >= this.animationSprites.Length)
+        {
+            _animationFrame = 0;
+        }
+
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Laser"))
+        {
+            this.killed.Invoke();
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+
+}
